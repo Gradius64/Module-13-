c9d68041d598b7fd70e9aa51bde0f03de4d7161b
Draca Baugh-Flowers <66185683+Gradius64@users.noreply.github.com>
2024-11-09 16:58:56 -0600

    Add files via upload

---
+++ SavedCandidates.tsx
@@ -0,0 +1,72 @@
+
+
+ const Candidate = () => {
+
+    /// Add Candidates in list
+
+    let addCandidates = [];
+    const listCandidates = localStorage.getItem('listCandidates');
+    if (typeof listCandidates === 'string') {
+
+        const list: string[] = ["Candidate1", "Candidate2"];
+
+        interface Candidate {
+            name: string;
+            value: number;
+       }
+
+       const Quantity: Candidate[] = [
+            { name: "William", value: 26 },
+            {name: "Zachary", value: 54 },
+            
+
+       ];
+
+       const matrix: number[][] = [
+            [2, 4, 6],
+
+            [10, 12, 14],
+
+       ];
+
+       const Area = localStorage.getItem('CandidateNumber');
+       if (typeof CandidateSearch === 'string') {
+
+        let Candidate = [2, 4, 'TypeA', 'TypeB'];
+
+       }
+
+
+    
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+}
+ }
